c7fee25ab05dda18402518112a018957ba2f5b33
seraph <quietsolitvde@gmail.com>
2026-04-03 20:48:40 +0700

    FINISHED PROHRJECT YAY.....

--- app.js
+++ app.js
@@ -17,3 +17,68 @@ console.log("Ur gay");
     (OR: It's a tie!)
 */
 
+
+let computerScore = 0;
+let playerScore = 0;
+
+function startGame() {
+    computerScore = 0;
+    playerScore = 0;
+    for ( let i=0 ; i<5 ; i++ ) {
+        playRound()
+    }
+    decideWinner();
+}
+
+function playRound() {
+    const computerChoice = getComputerChoice();
+    let playerChoice = prompt("Rock, paper, scissor??: ").toLowerCase();
+    while (playerChoice !== "rock"
+        && playerChoice !== "paper"
+        && playerChoice !== "scissor"
+    ) {
+        playerChoice = prompt("WRONG SPELLINGG, try again: ").toLowerCase();
+    }
+
+    checkRoundWinner(playerChoice, computerChoice);
+}
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random()*3) + 1;
+    switch (randomNumber) {
+        case (1):
+            return "rock";
+        case (2):
+            return "paper";
+        case (3):
+            return "scissor";
+    }
+}
+
+function checkRoundWinner(playerChoice, computerChoice) {
+    if (playerChoice === "rock" && computerChoice === "scissor"
+        || playerChoice === "paper" && computerChoice === "rock"
+        || playerChoice === "scissor" && computerChoice === "paper"
+    ) {
+        playerScore += 1;
+    } else if (playerChoice === computerChoice) {
+        //Do nothing
+    } else {
+        computerScore += 1;
+    }
+}
+
+function decideWinner() {
+    console.log(`FINAL RESULTS:
+        Player score: ${playerScore}
+        Computer score: ${computerScore}`)
+    if (playerScore > computerScore) {
+        console.log(`-> Player wins and is ${playerScore - computerScore} score higher than compyuterrrr !`);
+    } else if (playerScore < computerScore) {
+        console.log(`-> Computer wins and is ${computerScore - playerScore} score higher than playerrrr !`);
+    } else {
+        console.log(`-> It's a tie ! `);
+}
+}
+
+startGame()
