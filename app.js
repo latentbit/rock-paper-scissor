d4cd00f01ed1cc20bfe0b859750b0b6a93380dc6
seraph <quietsolitvde@gmail.com>
2026-04-03 19:25:58 +0700

    Add pseudocode

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("Ur gay");
+
+//RUN a function that plays 5 rounds
+    // RESET each player's score to 0
+    // GET player choice, put it in a variable
+    // GET computer choice
+        // GET a random number from a variable
+        // Turn the number to a human readable choice (Rock, paper, scissors)
+    // Determine who wins using conditional statements
+        //Increase the winner's score by one
+
+//Print the final results after 5 rounds, like so:
+/* FINAL RESULTS:
+    Player score: <...>
+    Computer score: <...>
+    => <winner> wins and is <...> score higher than <...> 
+    (OR: It's a tie!)
+*/
+
