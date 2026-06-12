9feca0a564a167b31be17f40092b73030c29aa05
seraph <quietsolitvde@gmail.com>
2026-06-12 10:31:08 +0700

    Refactor game logic using class

--- app.js
+++ app.js
@@ -1,72 +1,77 @@
-const playerOption = document.querySelector('div:nth-of-type(2)');
-const roundNumber = document.querySelector('span:first-of-type');
-const messageSection = document.querySelectorAll('div > p > span');
+class Game {
+    static playerOption = document.querySelector('div:nth-of-type(2)');
+    static roundNumber = document.querySelector('span:first-of-type');
+    static messageSection = document.querySelectorAll('div > p > span');
 
-
-playerOption.addEventListener('click', e => {
-    let playerChoice = undefined;
-    switch (e.target.id) {
-        case 'rock':
-            playerChoice = e.target.id;
-            break;
-        case 'paper':
-            playerChoice = e.target.id;
-            break;
-        case 'scissors':
-            playerChoice = e.target.id;
-            break;
-    }
-    if (playerChoice === 'rock' ||
-        playerChoice === 'paper' ||
-        playerChoice === 'scissors') {
-            decideWinner(getComputerChoice(), playerChoice);
+    getComputerChoice() {
+        const randomNumber = Math.floor(Math.random()*3 + 1);
+        switch (randomNumber) {
+            case 1:
+                return 'rock';
+            case 2:
+                return 'paper';
+            case 3:
+                return 'scissors';
         }
-})
-
-function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random()*3 + 1);
-    switch (randomNumber) {
-        case 1:
-            return 'rock';
-        case 2:
-            return 'paper';
-        case 3:
-            return 'scissors';
     }
-}
 
-function decideWinner(computerChoice, playerChoice) {
-    if (computerChoice === playerChoice) {
-        rewriteMessageSection('It\'s a draw!');
-    } else if (computerChoice === 'rock' && playerChoice === 'scissors' ||
-        computerChoice === 'paper' && playerChoice ==='rock' ||
-        computerChoice === 'scissors' && playerChoice === 'paper') {
-            rewriteMessageSection('Computer wins!', 0, 1)
-        } else {
-            rewriteMessageSection('Player wins!', 1, 0);
+    getPlayerChoice(e) {
+        let playerChoice = undefined;
+        switch (e.target.id) {
+            case 'rock':
+                playerChoice = e.target.id;
+                break;
+            case 'paper':
+                playerChoice = e.target.id;
+                break;
+            case 'scissors':
+                playerChoice = e.target.id;
+                break;
         }
-    roundNumber.textContent = +roundNumber.textContent + 1;
-    resetRound();
-}
+        if (playerChoice === 'rock' ||
+            playerChoice === 'paper' ||
+            playerChoice === 'scissors') {
+                this.decideWinner(this.getComputerChoice(), playerChoice);
+            }
+    }
 
-function rewriteMessageSection(message, playerScore, computerScore) {
-    messageSection[0].textContent = message;
-    
-    messageSection[1].textContent = 
-    +messageSection[1].textContent + (playerScore || 0);
+    decideWinner(computerChoice, playerChoice) {
+        if (computerChoice === playerChoice) {
+            this.rewriteMessageSection('It\'s a draw!');
+        } else if (computerChoice === 'rock' && playerChoice === 'scissors' ||
+            computerChoice === 'paper' && playerChoice ==='rock' ||
+            computerChoice === 'scissors' && playerChoice === 'paper') {
+                this.rewriteMessageSection('Computer wins!', 0, 1)
+            } else {
+                this.rewriteMessageSection('Player wins!', 1, 0);
+            }
+        Game.roundNumber.textContent = +Game.roundNumber.textContent + 1;
+        this.resetRound();
+    }
 
-    messageSection[2].textContent = 
-    +messageSection[2].textContent + (computerScore || 0);
-}
+    rewriteMessageSection(message, playerScore, computerScore) {
+        Game.messageSection[0].textContent = message;
+        
+        Game.messageSection[1].textContent = 
+        +Game.messageSection[1].textContent + (playerScore || 0);
 
-function resetRound() {            
-    const playerScore = +messageSection[1].textContent;
-    const computerScore = +messageSection[2].textContent;
-    if (playerScore === 5 || computerScore === 5) {
-        const winner = (playerScore - computerScore > 0) ? 'player' : 'computer';
-        messageSection[0].textContent = `${winner} has won! Game restarted.`;
-        messageSection[1].textContent = '';
-        messageSection[2].textContent = '';
-        roundNumber.textContent = 1;
+        Game.messageSection[2].textContent = 
+        +Game.messageSection[2].textContent + (computerScore || 0);
+    }
+
+    resetRound() {            
+        const playerScore = +Game.messageSection[1].textContent;
+        const computerScore = +Game.messageSection[2].textContent;
+        if (playerScore === 5 || computerScore === 5) {
+            const winner = (playerScore - computerScore > 0) ? 'player' : 'computer';
+            Game.messageSection[0].textContent = `${winner} has won! Game restarted.`;
+            Game.messageSection[1].textContent = '';
+            Game.messageSection[2].textContent = '';
+            Game.roundNumber.textContent = 1;
+        }
     }
 }
+
+const game = new Game;
+console.log(Game.prototype)
+Game.playerOption.addEventListener('click', game.getPlayerChoice.bind(game));
